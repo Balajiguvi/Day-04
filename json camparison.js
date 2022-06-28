b4a2de81fd82bc8f1a194735f2750dc15f9ea375
Balajiguvi <107595021+Balajiguvi@users.noreply.github.com>
2022-06-28 23:26:03 +0530

    day 04 task

--- json camparison.js
+++ json camparison.js
@@ -1,5 +1,8 @@
+// How to campare two JSON have same properties without order ?
+
 var obj1 = { name: "Person 1", age:5 };
 var obj2 = { age:5, name: "Person 1" };
+
  var JSON = true;
  if(Object.keys(obj1).length==Object.keys(obj2).length){
     for(let key in obj1){
