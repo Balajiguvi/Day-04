b25a59df445f633ddd83b7f674a46baa0702ae35
Balajiguvi <107595021+Balajiguvi@users.noreply.github.com>
2022-10-01 12:36:22 +0530

    Update json camparison.js

--- json camparison.js
+++ json camparison.js
@@ -1,4 +1,4 @@
-// How to campare two JSON have same properties without order ?
+// 1. How to campare two JSON have same properties without order ?
 
 var obj1 = { name: "Person 1", age:5 };
 var obj2 = { age:5, name: "Person 1" };
